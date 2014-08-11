3e4837ef81209e2a77e091ab006879e4a518d50c
Justin Howard <justin.howard@gobonzi.com>
2014-08-11 11:14:12 -0700

    added custom directory, fixed run.sh

---
+++ custom/example.sh
@@ -0,0 +1,2 @@
+# Put custom host specific setup here
+# It will be run after the shared environment setup
--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 # Run shared environment scripts
-for file in `pwd`"/share/*"; do
+for file in `pwd`/share/*.sh; do
+	echo $file
 	[ -f "$file" ] && . "$file"
 done
+
+# Run custom scripts
+if [ -d `pwd`/custom ]; then
+	for file in `pwd`/custom/*.sh; do
+		echo $file
+		[ -f "$file" ] && . "$file"
+	done
+fi
+
 unset file
