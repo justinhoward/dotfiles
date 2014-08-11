911435e415a7d4d6345e8c844a493b18525c1c4f
Justin Howard <justin.howard@gobonzi.com>
2014-08-11 10:51:25 -0700

    start zsh from bash if available

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -3,5 +3,17 @@
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
+# Start zsh if available
+if shopt -q login_shell && type zsh >/dev/null 2>&1; then
+	export SHELL=`which zsh`
+	if [ $? -eq 0 ]; then
+		exec $SHELL -l
+	else
+		echo 'Unable to locate zsh'
+	fi
+fi
+
 # Run shared environment
-[ -f ~/.environment ] && . ~/.environment
+if [[ -f ~/.environment ]]; then
+	. ~/.environment
+fi
