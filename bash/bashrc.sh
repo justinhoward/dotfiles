84e825eec08580f61855f23a01c5c2948d32060a
Justin Howard <justin.howard@gobonzi.com>
2014-08-11 11:24:03 -0700

    fixed run.sh, .bashrc will always run the shared environment

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -1,15 +1,15 @@
 # ~/.bashrc
 
-# If not running interactively, don't do anything
-[[ $- != *i* ]] && return
-
-# Start zsh if available
-if shopt -q login_shell && type zsh >/dev/null 2>&1; then
-	export SHELL=`which zsh`
-	if [ $? -eq 0 ]; then
-		exec $SHELL -l
-	else
-		echo 'Unable to locate zsh'
+# If running interactively, try to start zsh
+if [[ $- = *i* ]]; then
+	# Start zsh if available
+	if shopt -q login_shell && type zsh >/dev/null 2>&1; then
+		export SHELL=`which zsh`
+		if [ $? -eq 0 ]; then
+			exec $SHELL -l
+		else
+			echo 'Unable to locate zsh'
+		fi
 	fi
 fi
 
--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
+dir="$(dirname "$0")"
+
 # Run shared environment scripts
-for file in `pwd`/share/*.sh; do
-	echo $file
+for file in "$dir"/share/*.sh; do
 	[ -f "$file" ] && . "$file"
 done
 
 # Run custom scripts
-if [ -d `pwd`/custom ]; then
-	for file in `pwd`/custom/*.sh; do
-		echo $file
+if [ -d "$dir"/custom ]; then
+	for file in "$dir"/custom/*.sh; do
 		[ -f "$file" ] && . "$file"
 	done
 fi
 
-unset file
+unset dir file
