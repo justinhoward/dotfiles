7870d886afa48cfcfa61a785d56a1642a78c4b0e
Justin Howard <jmhoward0@gmail.com>
2014-09-22 23:18:08 -0700

    right prompt working

--- modules/prezto_setup/prompt.sh
+++ modules/prezto_setup/prompt.sh
@@ -1,4 +1,6 @@
 # Set the prompt theme to load.
 # Setting it to 'random' loads a random theme.
 # Auto set to 'off' on dumb terminals.
+zstyle ':prezto:module:prompt:justinhoward' default_user "justin_howard"
+
 zstyle ':prezto:module:prompt' theme 'justinhoward'
