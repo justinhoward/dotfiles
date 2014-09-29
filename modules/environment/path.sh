bf3d1a306f695dd869df10fa29969ef085800f2a
Justin Howard <justin.howard@gobonzi.com>
2014-09-29 16:46:20 -0700

    added common path directories

--- modules/environment/path.sh
+++ modules/environment/path.sh
@@ -1,19 +1,36 @@
-export PATH=$HOME/bin:/usr/local/bin:$HOME/.composer/vendor/bin:$PATH
+PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
-typeset -gU cdpath fpath mailpath path
+# PHP Composer
+if which composer >/dev/null; then
+    if [[ -d "$COMPOSER_HOME" ]]; then
+        PATH="$COMPOSER_HOME/vendor/bin:$PATH"
+    else if [[ -d "$HOME/.composer/vendor/bin" ]]
+        PATH="$HOME/.composer/vendor/bin:$PATH"
+    fi
+fi
+
+# Ruby gems
+if which ruby >/dev/null && which gem >/dev/null; then
+    PATH="$(ruby -rubygems -e 'puts Gem.user_dir')/bin:$PATH"
+fi
+
+if [[ -d "$HOME/bin" ]]; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+export PATH
 
 if [ $DOTFILES_SHELL = "zsh" ]; then
     # Ensure path arrays do not contain duplicates.
     typeset -gU cdpath fpath mailpath path
 
     # Set the the list of directories that cd searches.
-    # cdpath=(
-    #   $cdpath
-    # )
+    cdpath=(
+      $cdpath
+    )
 
     # Set the list of directories that Zsh searches for programs.
     path=(
-      /usr/local/{bin,sbin}
       $path
     )
 
