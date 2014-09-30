802eec3d5e05622ea228d4739f12c4a9786cbad9
Justin Howard <justin.howard@gobonzi.com>
2014-09-30 10:27:19 -0700

    cleaned up context prompt section

--- modules/prezto_setup/preinit/prompt.sh
+++ modules/prezto_setup/preinit/prompt.sh
@@ -2,5 +2,6 @@
 # Setting it to 'random' loads a random theme.
 # Auto set to 'off' on dumb terminals.
 zstyle ':prezto:module:prompt:justinhoward' default_user "justin_howard"
+zstyle ':prezto:module:prompt:justinhoward' default_hostname "justinbox"
 
 zstyle ':prezto:module:prompt' theme 'justinhoward'
