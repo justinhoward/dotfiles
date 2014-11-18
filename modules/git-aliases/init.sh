95c9be8792154e65814d80dbff643ddfd912ffed
Justin Howard <justin.howard@gobonzi.com>
2014-11-18 12:04:24 -0800

    added submodule update alias

--- modules/git-aliases/init.sh
+++ modules/git-aliases/init.sh
@@ -4,3 +4,6 @@ alias glf='git log --topo-order --name-status --pretty=format:${_git_log_medium_
 # Tag (t)
 alias gt='git tag --sort v:refname'
 alias gtl='git tag --sort v:refname | head -1'
+
+# Submodule (S)
+alias gSu='git submodule update'
