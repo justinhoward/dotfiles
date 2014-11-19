a3f45f21b75dfa33391da7ea6dfaeb5ae03ae824
Justin Howard <justin.howard@gobonzi.com>
2014-11-19 09:59:14 -0800

    added git branch remote, fixed latest tag (gtl)

--- modules/git-aliases/init.sh
+++ modules/git-aliases/init.sh
@@ -3,13 +3,14 @@ alias glf='git log --topo-order --name-status --pretty=format:${_git_log_medium_
 
 # Tag (t)
 alias gt='git tag --sort v:refname'
-alias gtl='git tag --sort v:refname | head -1'
+alias gtl='git tag --sort -v:refname | head -1'
 
 # Submodule (S)
 alias gSu='git submodule update --recursive'
 
 # Branch (b)
 alias gba='git branch -a'
+alias gbr='git branch -r'
 
 # Index (i)
 alias giA='git add --all'
