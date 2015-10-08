49ca3611cc069c2656efc80fe8d84ad1e1f5856b
Justin Howard <jmhoward0@gmail.com>
2015-10-07 18:56:02 -0700

    Vim plugins only install once
    - Plugins are only installed when VimPlug is installed
    - PlugInstall is run on update

--- modules/vim/install.sh
+++ modules/vim/install.sh
@@ -6,9 +6,11 @@ dsymlink modules/vim/vimrc ~/.vimrc
 [[ ! -e "$HOME/.vim/undo" ]] && mkdir "$HOME/.vim/undo"
 
 if [[ ! -e "$HOME/.vim/autoload/plug.vim" ]]; then
+  echo 'Installing VimPlug...'
   curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+  echo 'Installing vim plugins...'
+  nohup vim -c 'PlugInstall'  -c 'qall' >/dev/null 2>&1
 fi
 
-echo 'Installing vim plugins...'
-nohup vim -c 'PlugInstall'  -c 'qall' >/dev/null 2>&1
