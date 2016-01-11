e338540feb9f183f24a3cae1fd84d4710486276c
Justin Howard <jmhoward0@gmail.com>
2016-01-11 11:28:38 -0800

    Replace sudo alias with a more sophisitcated alias resolver

--- modules/sudo/init.sh
+++ modules/sudo/init.sh
@@ -1,3 +1,33 @@
-# The space at the end of this alias allows recursively expanding the alias so
-# that user aliases can be used with sudo.
-dcheck sudo && alias sudo='sudo '
+dcheck sudo || return
+
+function _sudo_resolve_aliases() {
+  local -a args
+  while getopts ':Abc:C:Eeg:Hh:iKklnPp:SsU:u:Vv-' opt; do
+    case "$opt" in
+      -)
+        break ;;
+      \?|:)
+        args+=("-$OPTARG")
+        ;;
+      *)
+        args+=("-$opt")
+        args+=("$OPTARG")
+        ;;
+    esac
+    shift $((OPTIND-1))
+  done
+
+  result=`alias "$1" 2> /dev/null`
+  [[ $? -eq 0 ]] && command=(`echo "$result" | head -n1 | sed "s/[^=]*='\(.*\)'/\1/"`) || command=("$1")
+  shift
+
+  case "${command[1]}" in
+    nocorrect|noglob)
+      command=("${command[@]:1}")
+      ;;
+  esac
+
+  \sudo "${args[@]}" "${command[@]}" "$@"
+}
+
+alias sudo='_sudo_resolve_aliases'
