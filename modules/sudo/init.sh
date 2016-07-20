f9505c2e1de04a513957c1813677ac275db8176c
Justin Howard <jmhoward0@gmail.com>
2016-07-20 15:33:08 -0700

    Fix sudo module

--- modules/sudo/init.sh
+++ modules/sudo/init.sh
@@ -2,32 +2,43 @@ dcheck sudo || return
 
 function _sudo_resolve_aliases() {
   local -a args
-  while getopts ':Abc:C:Eeg:Hh:iKklnPp:SsU:u:Vv-' opt; do
+
+  while getopts ':c:C:g:h:p:U:u:-' opt; do
     case "$opt" in
+      # Catch -- argument and stop parsing
+      # getopts also automatically stops parsing if it encounters a non-option
+      # argument
       -)
-        break ;;
+        break
+        ;;
+      # Record all unmatched options and options missing arguments
       \?|:)
         args+=("-$OPTARG")
         ;;
+      # Record all options with arguments
       *)
         args+=("-$opt")
         args+=("$OPTARG")
         ;;
     esac
-    shift $((OPTIND-1))
   done
 
-  result=`alias "$1" 2> /dev/null`
-  [[ $? -eq 0 ]] && command=(`echo "$result" | head -n1 | sed -e "s/[^=]*=\(.*\)/\1/" -e "s/^'//" -e "s/'$//" `) || command=("$1")
+  # Shift off all unparsed options
+  shift $((OPTIND-1))
+
+  # Expand any aliases of the command (argument $1)
+  result=$(alias "$1" 2> /dev/null)
+  [[ $? -eq 0 ]] && command=($(echo "$result" | head -n1 | sed -e "s/[^=]*=\(.*\)/\1/" -e "s/^'//" -e "s/'$//" )) || command=("$1")
   shift
 
+  # Remove nocorrect and noglob since those don't work with sudo
   case "${command[1]}" in
     nocorrect|noglob)
       command=("${command[@]:1}")
       ;;
   esac
 
-  \sudo "${args[@]}" "${command[@]}" "$@"
+  /usr/bin/env sudo "${args[@]}" "${command[@]}" "$@"
 }
 
 alias sudo='_sudo_resolve_aliases'
