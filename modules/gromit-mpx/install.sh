02a3eaf56045794d6862a61d88d4aced50311bf4
Justin Howard <jmhoward0@gmail.com>
2019-03-06 22:59:37 -0800

    Add gromit-mpx module for on-screen drawing

---
+++ modules/gromit-mpx/install.sh
@@ -0,0 +1,4 @@
+if [[ "$dotfiles_platform" == 'linux' ]]; then
+  dsymlink modules/gromit-mpx/gromit-mpx.cfg ~/.config/gromit-mpx.cfg
+  dsymlink modules/gromit-mpx/gromit-mpx.ini ~/.config/gromit-mpx.ini
+fi
