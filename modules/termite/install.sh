4bfff2b715a540a26b8a360f8320b216e919dfc5
Justin Howard <jmhoward0@gmail.com>
2019-03-18 15:21:50 -0700

    Set termite as default file manager

--- modules/termite/install.sh
+++ modules/termite/install.sh
@@ -1,3 +1,7 @@
 if [[ "$dotfiles_platform" == 'linux' ]]; then
   dsymlink modules/termite/installed-config ~/.config/termite
+
+  # Set termite as default file manager
+  dsymlink modules/termite/termite-dir.desktop ~/.local/share/applications/termite-dir.desktop
+  xdg-mime default termite-dir.desktop inode/directory
 fi
