d013514067ffe7d26a9b5efbfe1ed79c92849842
Justin Howard <jmhoward0@gmail.com>
2019-05-15 13:17:45 -0700

    Allow homebrew install of chruby

--- modules/chruby/init.sh
+++ modules/chruby/init.sh
@@ -1,4 +1,10 @@
-if [ -f /usr/share/chruby/chruby.sh ] && [[ "$dotfiles_shell" =~ (zsh|bash) ]]; then
-  source /usr/share/chruby/chruby.sh
-  source /usr/share/chruby/auto.sh
+if [ -d /usr/local/opt/chruby ]; then
+  _chruby_path=/usr/local/opt/chruby
+else
+  _chruby_path=/usr
+fi
+
+if [ -f "$_chruby_path"/share/chruby/chruby.sh ] && [[ "$dotfiles_shell" =~ (zsh|bash) ]]; then
+  source "$_chruby_path"/share/chruby/chruby.sh
+  source "$_chruby_path"/share/chruby/auto.sh
 fi
