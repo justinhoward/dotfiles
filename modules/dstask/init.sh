17f8427ec942c4a85b1d11b85cf3e13e2ebd9aa9
Justin Howard <jmhoward0@gmail.com>
2022-04-14 20:15:20 -0700

    Add more dstask aliases

--- modules/dstask/init.sh
+++ modules/dstask/init.sh
@@ -2,11 +2,14 @@ alias t='dstask'
 alias ta='t add'
 alias tl='t log'
 alias to='t open'
+alias tn='t note'
 alias tm='t modify'
 alias te='t edit'
 alias ts='t start'
 alias td='t done'
+alias tD='t remove'
 alias tp='t sync' # Push
+alias tc='t context'
 
 # View
 alias tvr='t show-resolved'
