2909b8e02623d175ae22b36094ed06bfb20800a4
Justin Howard <jmhoward0@gmail.com>
2022-05-27 16:41:00 -0700

    Add color support for fzf/fd

--- modules/fzf/init.sh
+++ modules/fzf/init.sh
@@ -1,7 +1,8 @@
 dcheck fzf || return
 
 if dcheck fd; then
-  export FZF_DEFAULT_COMMAND="fd --exclude .git --type f --no-ignore --hidden"
+  export FZF_DEFAULT_COMMAND="fd --exclude .git --type f --no-ignore --hidden --color=always"
+  export FZF_DEFAULT_OPTS="--ansi"
 else
   find_cmd=find
   dcheck gfind && find_cmd=gfind
