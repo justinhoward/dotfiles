74c5f5ac7e1aa4f85de613ad511ac240b64dd21a
Justin Howard <jmhoward0@gmail.com>
2022-06-02 23:03:26 -0700

    Add exa module

---
+++ modules/exa/init.sh
@@ -0,0 +1,5 @@
+alias ls=exa
+alias ll='exa -lF --git --icons --group'
+alias l='exa -lF --git --icons --no-user --no-permissions --no-filesize --no-time'
+alias lt='l -TL2'
+alias la='ll -a'
---
+++ modules/exa/install.sh
@@ -0,0 +1,1 @@
+drecommend exa
