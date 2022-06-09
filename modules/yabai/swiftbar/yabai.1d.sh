b4e6c1bd5e46cd240a74f41078befdc411732a1e
Justin Howard <jmhoward0@gmail.com>
2022-06-09 13:27:19 -0700

    Fixes for yabai swiftbar plugin

--- modules/yabai/swiftbar/yabai.1d.sh
+++ modules/yabai/swiftbar/yabai.1d.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/usr/bin/env bash
 
 # <bitbar.title>Yabai-Spaces</bitbar.title>
 # <bitbar.version>v1.5</bitbar.version>
@@ -29,22 +29,11 @@ if [[ "$1" = "restart" ]]; then
   brew services restart skhd
 fi
 
-# Avaliable colors and their associated ANSI color codes. Don't change your color here unless you know what you're doing!
-BLACK='\033[0;30m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-MAGENTA='\033[0;35m'
-CYAN='\033[0;36m'
-WHITE='\033[0;37m'
-RESET='\033[m'
-
 # Change your color here to one of the options above! $COLOR will be the color of the current space.
 # $DEFAULT will be the color of the other spaces
 
-COLOR=$CYAN
-DEFAULT=$WHITE
+COLOR=$'\e[30;47m'
+DEFAULT=$'\e[0;37m'
 
 # Change your icons here! $LEFT and $RIGHT refer to the icons on either end of the bar and $DIV refers to the icon between spaces.
 # e.x. if $LEFT='\〈', $RIGHT='\〉', and $DIV='|', your bar would look like this ⟨ ｜ ⟩.
@@ -52,16 +41,10 @@ DEFAULT=$WHITE
 # List of all Unicode charecters can be found here: https://unicode-table.com/en/
 # P.S. Check out the "Math" and "Misc" sections for cool and unique symbols to use!ˈ
 
-LEFT=''
-RIGHT=''
-DIV=' '
-SELECTLEFT=' '
-SELECTRIGHT=' '
-
 # Change your font here! Make sure you only change the stuff on the right of the '='. You can get the exact name of your font from the FontBook application on MacOS.
 # A monowidth font (such as FuraCode) is recomended. Ligatures are a plus!
 
-FONT=(font=Hack Nerd Font Regular trim=true size=12 ansi=true)
+FONT="font='Hack Nerd Font' trim=true size=12 ansi=true"
 
 # Change your space indicies here! Add a space between the indicies if you would like padding.
 # Feel free to use this as a template and increase the $SPACES array as you see fit. 
@@ -70,12 +53,6 @@ FONT=(font=Hack Nerd Font Regular trim=true size=12 ansi=true)
 
 # SPACES=('•' '•' '•' '•' '•' '•' '•' '•' '•' '•' '•' '•' '•' '•' '•' '•')
 SPACES=({1..16})
-
-# Choose your style. Options "NUMBER" and "CUSTOM".
-# NUMBER uses arabic neumerals e.g. 1, 2, 3, etc...
-# CUSTOM uses your array elements from SPACES. e.g. I, II, III, etc...
-
-STYLE="CUSTOM"
 
 # Choose weather or not you want to see the type of the current space. i.e. BSP, STACK, or FLOAT
 # Options, `true` or `false`
@@ -97,70 +74,73 @@ WINLEFT=" "
 WINRIGHT=" "
 
 # Set left and right separators for displays
-DISPLAYLEFT="["
-DISPLAYRIGHT="]"
+DISPLAYLEFT='['
+DISPLAYRIGHT='] '
 
 # -------------------------------------------------------------------------#
 # Don't edit anything below this point if you dont know what you're doing! #
 # -------------------------------------------------------------------------#
 
-CURRENT=$(yabai -m query --spaces --display | jq 'map(select(."has-focus" == true))[-1].index')
-TOTAL=$(yabai -m query --spaces | jq '. | length')
-STRING="$DEFAULT$LEFT" 
-i=1
+CURRENT="$(yabai -m query --spaces --display | jq -r 'map(select(."has-focus" == true))[-1].index')"
+SPACES_COUNT="$(yabai -m query --spaces | jq -r '. | length')"
 
-for DISPLAY in {1..$(yabai -m query --displays | jq '. | length')}; do 
-  VAR=$(yabai -m query --displays --display $DISPLAY | jq '.spaces | .[0]')
-  SPACES[VAR]=$DISPLAYLEFT$SPACES[VAR]
-done
-
-for DISPLAY in {1..$(yabai -m query --displays | jq '. | length')}; do 
-  VAR=$(yabai -m query --displays --display $DISPLAY | jq '.spaces | .[-1]')
-  SPACES[VAR]=$SPACES[VAR]$DISPLAYRIGHT
-done
-
-if [ "$STYLE" = "NUMBER" ]; then  
-  while [ $i -le $TOTAL ]; do
-    if [ $i -eq $CURRENT ]; then
-      STRING+="$COLOR$SELECTLEFT${i}$SELECTRIGHT$DEFAULT"
-      i=$(( $i + 1 ))
-    else
-      STRING+="$DIV${i}$DIV"
-      i=$(( $i + 1 ))
-    fi
-  done
-elif [ "$STYLE" = "CUSTOM" ]; then
-  while [ $i -le $TOTAL ] || [ $i -eq $TOTAL ]; do
-    if [ $i -eq $CURRENT ]; then
-      STRING+="$COLOR$SELECTLEFT${SPACES[i]}$SELECTRIGHT$DEFAULT"
-      i=$(( $i + 1 ))
-    else
-      STRING+="$DIV${SPACES[i]}$DIV"
-      i=$(( $i + 1 ))
-    fi
-  done
+if [ "$SPACETYPE" = true ]; then
+  TYPE="$(yabai -m query --spaces --space | jq -r .type)"
 fi
 
-FINALSTRING=$(echo "$STRING$RIGHT" | tr -s "$DIV")
+if [ "$WINTYPE" = true ]; then
+  WINDOW="$(yabai -m query --windows --window | jq -r .floating)" 2> /dev/null
+fi
 
-TYPE=$(yabai -m query --spaces --space | jq .type)
+readarray -t DISPLAY_SPACES < <(yabai -m query --displays | jq -r '.[].spaces |join(" ")')
+STRING="$DEFAULT$LEFT" 
 
-if [[ $TYPE == *"bsp"* ]]; then
+FIRST_SPACES=()
+LAST_SPACES=()
+for DISPLAY in "${DISPLAY_SPACES[@]}"; do 
+  readarray -d ' ' -t THIS_SPACES <<< "$DISPLAY"
+  FIRST_SPACES["${THIS_SPACES[0]}"]=1
+  LAST_SPACES["${THIS_SPACES[${#THIS_SPACES[@]} - 1]}"]=1
+done
+
+for i in $(seq 0 $(( SPACES_COUNT - 1))); do
+  SPACE_STR="${SPACES[$i]}"
+  if [ "$i" -eq "$CURRENT" ]; then
+    SPACE_STR="$COLOR$SELECTLEFT$SPACE_STR$SELECTRIGHT$DEFAULT"
+  else
+    SPACE_STR="$DIV$SPACE_STR"
+  fi
+
+  if [ -n "${FIRST_SPACES[i + 1]}" ]; then
+    SPACE_STR="$DISPLAYLEFT$SPACE_STR"
+  fi
+
+  if [ -n "${LAST_SPACES[i + 1]}" ]; then
+    SPACE_STR="$SPACE_STR$DISPLAYRIGHT"
+  else
+    SPACE_STR+=' '
+  fi
+
+  STRING+="$SPACE_STR"
+done
+
+FINALSTRING="$STRING$RIGHT"
+
+
+if [[ $TYPE == bsp ]]; then
   TYPE=$BSP
-elif [[ $TYPE == *"stack"* ]]; then
+elif [[ $TYPE == stack ]]; then
   TYPE=$STACK
-elif [[ $TYPE == *"float"* ]]; then
+elif [[ $TYPE == float ]]; then
   TYPE=$FLOAT
 else
   TYPE=''
 fi
 
-WINDOW=$(yabai -m query --windows --window | jq .floating) 2> /dev/null
-
-if ! [ -z "$WINDOW" ] && [[ $WINDOW -eq "0" ]]; then
-  WINDOW=$WINMANAGED
-elif ! [ -z "$WINDOW" ] && [[ $WINDOW -eq "1" ]]; then
-  WINDOW=$WINFLOAT
+if [ -n "$WINDOW" ] && [[ "$WINDOW" -eq "0" ]]; then
+  WINDOW="$WINMANAGED"
+elif [ -n "$WINDOW" ] && [[ "$WINDOW" -eq "1" ]]; then
+  WINDOW="$WINFLOAT"
 else
   WINDOW=''
 fi
@@ -175,7 +155,7 @@ elif [[ "$SPACETYPE" = false ]] && [[ "$WINTYPE" = false ]]; then
   FINAL=$FINALSTRING
 fi
 
-echo "$FINAL" "| ${FONT[@]}"
+echo "$FINAL |" "${FONT[@]}"
 
 echo "---"
 echo "Restart yabai & skhd | bash='$0' param1=restart terminal=false"
