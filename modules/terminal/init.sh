29d44c66477b6a98f142ab17b4a356f6810a652b
Justin Howard <jmhoward0@gmail.com>
2022-07-06 20:44:17 -0700

    Fix TERM when running kitty

--- modules/terminal/init.sh
+++ modules/terminal/init.sh
@@ -1,4 +1,6 @@
-if [[ -n "$dotfiles_virtual" ]]; then
+if [[ "$TERM" = xterm-kitty ]]; then
+  true
+elif [[ -n "$dotfiles_virtual" ]]; then
   export TERM='xterm-256color'
 else
   export TERM='xterm-color'
