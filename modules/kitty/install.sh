596af1f7a9793b0c48825620fc1f776364cbe099
Justin Howard <jmhoward0@gmail.com>
2022-08-23 16:51:46 -0700

    Symlink kitty install instead of copy

--- modules/kitty/install.sh
+++ modules/kitty/install.sh
@@ -2,24 +2,5 @@
 
 dconfig kitty
 
-kitty_tmp="$(mktemp)"
-kitty_conf="$HOME/.config/kitty/kitty.conf"
-envsubst < modules/kitty/rc/kitty.conf > "$kitty_tmp"
-
-if [ -f ~/.config/kitty/kitty.conf ]; then
-  if [ "$(md5sum "$kitty_tmp" | cut -d' ' -f1)" != "$(md5sum "$kitty_conf" | cut -d' ' -f1)" ]; then
-    dbackup "$kitty_conf"
-    kitty_write=1
-  fi
-else
-  kitty_write=1
-fi
-
-if [ -n "$kitty_write" ]; then
-  echo "Writing $kitty_conf"
-  mkdir -p "$(dirname "$kitty_conf")"
-  cp -f "$kitty_tmp" "$kitty_conf"
-fi
-
-rm "$kitty_tmp"
-unset kitty_tmp kitty_conf kitty_write
+dsymlink modules/kitty/rc/kitty.conf ~/.config/kitty/kitty.conf
+echo "font_size ${dotfiles_kitty_font_size}" > ~/.config/kitty/font_size.conf
