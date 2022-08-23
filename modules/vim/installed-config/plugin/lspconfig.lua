caa35ab20da5b50f83310ae32aaeb88c2b655f1e
Justin Howard <jmhoward0@gmail.com>
2022-08-23 16:52:11 -0700

    Fix vim diagnostics info sign

--- modules/vim/installed-config/plugin/lspconfig.lua
+++ modules/vim/installed-config/plugin/lspconfig.lua
@@ -4,7 +4,7 @@ local cmp_nvim_lsp = require('cmp_nvim_lsp')
 
 vim.fn.sign_define("DiagnosticSignError", { text = ' ', texthl = 'DiagnosticSignError' })
 vim.fn.sign_define("DiagnosticSignWarn", { text = ' ', texthl = 'DiagnosticSignWarn' })
-vim.fn.sign_define("DiagnosticSignInformation", { text = ' ', texthl = 'DiagnosticSignInfo' })
+vim.fn.sign_define("DiagnosticSignInfo", { text = ' ', texthl = 'DiagnosticSignInfo' })
 vim.fn.sign_define("DiagnosticSignHint", { text = ' ', texthl = 'DiagnosticSignHint' })
 
 vim.diagnostic.config {
