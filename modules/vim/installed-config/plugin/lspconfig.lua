4967d55679d5fee3528c5bc3543274a5c326e130
Justin Howard <jmhoward0@gmail.com>
2022-12-16 22:24:23 -0800

    Add codespell to vim

--- modules/vim/installed-config/plugin/lspconfig.lua
+++ modules/vim/installed-config/plugin/lspconfig.lua
@@ -144,6 +144,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.code_actions.shellcheck,
+    null_ls.builtins.diagnostics.codespell,
     null_ls.builtins.diagnostics.cppcheck,
     null_ls.builtins.diagnostics.cfn_lint,
     null_ls.builtins.diagnostics.erb_lint.with({
@@ -175,6 +176,7 @@ null_ls.setup({
         require("null-ls").builtins.formatting.erb_lint._opts.args
       )
     }),
+    null_ls.builtins.formatting.codespell,
     null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.diagnostics.rubocop.with({
