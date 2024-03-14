e29521cc49516fb74a5e714da2752674e90b1cea
Justin Howard <jmhoward0@gmail.com>
2024-03-13 19:28:31 -0600

    Customize forgit aliases

--- modules/forgit/init.sh
+++ modules/forgit/init.sh
@@ -3,31 +3,25 @@
 dcheck git-forgit || exit
 dcheck brew || exit
 
-export FORGIT_NO_ALIASES=1
-
-if [ "$dotfiles_shell" = bash ]; then
-  [ -f "$(brew --prefix)"/share/forgit/forgit.plugin.sh ] && . "$(brew --prefix)"/share/forgit/forgit.plugin.sh
-elif [ "$dotfiles_shell" = zsh ]; then
-  [ -f "$(brew --prefix)"/share/forgit/forgit.plugin.zsh ] && . "$(brew --prefix)"/share/forgit/forgit.plugin.zsh
-fi
+export FORGIT_LOG_GRAPH_ENABLE=false
 
 alias G=git-forgit
-alias Gia='G add'
-alias Gbd='G branch_delete'
-alias Gco='G checkout_branch'
-alias Gcc='G checkout_commit'
-alias Gcf='G checkout_file'
-alias Gct='G checkout_tag'
-alias Gcp='G cherry_pick'
-alias Gcpb='G cherry_pick_from_branch'
-alias Gwc='G clean'
-alias Gwd='G diff'
-alias Gid='G diff --staged'
-alias Gfu='G fixup'
-alias Gi='G ignore'
-alias Gl='G log'
-alias Gr='G rebase'
-alias Gir='G reset_head'
-alias Gcr='G revert_commit'
-alias Gs='G stash_push'
-alias Gsd='G stash_show'
+alias Gia='git-forgit add'
+alias Gbd='git-forgit branch_delete'
+alias Gco='git-forgit checkout_branch'
+alias Gcc='git-forgit checkout_commit'
+alias Gcf='git-forgit checkout_file'
+alias Gct='git-forgit checkout_tag'
+alias Gcp='git-forgit cherry_pick'
+alias Gcpb='git-forgit cherry_pick_from_branch'
+alias Gwc='git-forgit clean'
+alias Gwd='git-forgit diff'
+alias Gid='git-forgit diff --staged'
+alias Gfu='git-forgit fixup'
+alias Gi='git-forgit ignore'
+alias Gl='git-forgit log'
+alias Gr='git-forgit rebase'
+alias Gir='git-forgit reset_head'
+alias Gcr='git-forgit revert_commit'
+alias Gs='git-forgit stash_push'
+alias Gsd='git-forgit stash_show'
