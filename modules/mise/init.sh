bb4fc11f3925760ce5591e88b22ca51ad213b31d
Justin Howard <jmhoward0@gmail.com>
2024-03-18 12:06:00 -0600

    Enable mise completion

--- modules/mise/init.sh
+++ modules/mise/init.sh
@@ -4,6 +4,8 @@ dcheck mise || return
 
 if [ "$dotfiles_shell" = zsh ]; then
   eval "$(mise activate zsh)"
+  eval "$(mise completion zsh)"
 elif [ "$dotfiles_shell" = bash ]; then
   eval "$(mise activate bash)"
+  eval "$(mise completion bash)"
 fi
