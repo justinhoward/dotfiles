caea07e99cf52895e751af12ef1699f8ff0cdc32
Justin Howard <jmhoward0@gmail.com>
2024-06-11 11:28:54 -0600

    Recommend git CLI tools

--- modules/git/install.sh
+++ modules/git/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+drecommend 'git'
+drecommend 'delta'
+
 dsymlink modules/git/installed-config/gitignore-global ~/.config/git/ignore
 
 git config --global include.path "$DOTFILES_PATH"/modules/git/installed-config/default
