a2fe0e0f1e9c7ee522a5085b2d8c1d000ffe3238
Justin Howard <jmhoward0@gmail.com>
2025-07-25 12:12:56 -0600

    Only load homebrew into path in interactive

--- modules/homebrew/init.sh
+++ modules/homebrew/init.sh
@@ -2,11 +2,14 @@
 
 if [ "$dotfiles_platform" = 'osx' ] && [ -e /opt/homebrew/bin/brew ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
-  if [ "$dotfiles_shell" = zsh ]; then
-    FPATH="${HOMEBREW_PREFIX}/share/zsh/site-functions:${FPATH}"
-  elif [ "$dotfiles_shell" = bash ]; then
-    for completion in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
-      [ -r "${completion}" ] && source "${completion}"
-    done
+
+  if [ -n "$dotfiles_interactive" ]; then
+    if [ "$dotfiles_shell" = zsh ]; then
+      FPATH="${HOMEBREW_PREFIX}/share/zsh/site-functions:${FPATH}"
+    elif [ "$dotfiles_shell" = bash ]; then
+      for completion in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
+        [ -r "${completion}" ] && source "${completion}"
+      done
+    fi
   fi
 fi
