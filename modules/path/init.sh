e312a0c34d55798ad186c81d6cb15ac0bea7b5ab
Justin Howard <jmhoward0@gmail.com>
2025-07-25 12:13:58 -0600

    Add swiftly (swift) toolchain to path

--- modules/path/init.sh
+++ modules/path/init.sh
@@ -16,6 +16,11 @@ if [ -d "$HOME/.cargo/bin" ]; then
   PATH="$HOME/.cargo/bin:$PATH"
 fi
 
+# Swift (swiftly) toolchain bin
+if [ -d "$HOME/.swiftly/bin" ]; then
+  PATH="$HOME/.swiftly/bin:$PATH"
+fi
+
 if [ -d "$HOME/.local/bin" ]; then
   PATH="$HOME/.local/bin:$PATH"
 fi
