abd5ffcfc217eee6ae6baad5ca0a10bc1980de0c
Justin Howard <jmhoward0@gmail.com>
2025-07-25 20:25:07 -0600

    Disable opencode for now

--- modules/vim/installed-config/plugin/opencode.lua
+++ modules/vim/installed-config/plugin/opencode.lua
@@ -1,5 +1,22 @@
--- require('opencode').setup()
-
+-- sudo-tee/opencode.nvim
+-- require('opencode').setup({
+--   keymap = {
+--     global = {
+--       toggle = '<leader>aa', -- Open opencode. Close if opened
+--       open_input = '<leader>ai', -- Opens and focuses on input window on insert mode
+--       open_input_new_session = '<leader>aI', -- Opens and focuses on input window on insert mode. Creates a new session
+--       open_output = '<leader>ao', -- Opens and focuses on output window
+--       toggle_focus = '<leader>at', -- Toggle focus between opencode and last window
+--       close = '<leader>aq', -- Close UI windows
+--       select_session = '<leader>as', -- Select and load a opencode session
+--       configure_provider = '<leader>ap', -- Quick provider and model switch from predefined list
+--       diff_open = '<leader>ad', -- Opens a diff tab of a modified file since the last opencode prompt
+--       diff_next = '<leader>a]', -- Navigate to next file diff
+--       diff_prev = '<leader>a[', -- Navigate to previous file diff
+--       diff_close = '<leader>ac', -- Close diff view tab and return to normal editing
+--     }
+--   }
+-- })
 
 ---------------------------------------------
 -- NickvanDyke/opencode.nvim
